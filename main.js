32d4abf7a96940d30e9f233aa64b1c8cf347bf96
Ved17-11-09 <90040144+Ved17-11-09@users.noreply.github.com>
2022-01-14 08:25:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function preload() {
+
+}
+
+function setup() {
+ canvas = createCanvas(300,300)
+    canvas.center() ;
+    video = createCapture(VIDEO) ;
+    video.size(300,300) ;
+    video.hide() ;
+
+   poseNet = ml5.poseNet(video, modelLoaded) ;
+   poseNet.on('pose' , gotPoses) ;
+}
+
+function gotPoses(results) {
+    if (results.length >=0) {
+       console.log(results) ;
+       console.log("nose.x = "+results[0].pose.nose.x);
+       console.log("nose.y = "+results[0].pose.nose.y);
+      // console.log("eye.x = "+results[0].pose.leftEye.x);
+      // console.log("eye.y = "+results[0].pose.leftEye.y);
+      // console.log("eye.x = "+results[0].pose.rightEye.x);
+      // console.log("eye.y = "+results[0].pose.rightEye.y);
+    }
+}
+
+function modelLoaded() {
+    console.log("PoseNet Is Initialized") ;
+}
+
+function draw() {
+    image(video,0,0,300,300) ;
+}
+
+function take_snapshot() {
+    save("baburao.png") ;
+}
